da501e10840aa7dbf9d5d03f5ac238244dd76af7
rudrajosh <120940310+rudrajosh@users.noreply.github.com>
2023-04-14 12:14:50 +0000

    Pending changes exported from your codespace

---
+++ mm.py
@@ -0,0 +1,47 @@
+
+matrix_A_rows = int(input('ENter number of rows for matrix-A: '))
+matrix_A_cols  = int(input('ENter number of columns for matrix-A: '))
+
+print()
+
+matrix_B_rows = int(input('ENter number of rows for matrix-B: '))
+matrix_B_cols  = int(input('ENter number of columns for matrix-B: '))
+
+print() 
+
+if matrix_A_cols == matrix_B_rows:
+    print('Enter values for matrix A')
+    matrix_A = [[int(input(f"column {j+1} -> ENter {i+1} element:")) for j in range(matrix_A_cols)] for i in range(matrix_A_rows) ]
+
+    print() 
+
+    print('Enter values for matrix B ')
+    matrix_B = [[int(input(f"column {j+1} -> ENter {i+1} element:")) for j in range(matrix_B_cols)] for i in range(matrix_B_rows) ]
+
+    print() 
+
+    print('Matrix-A :')
+    for i in matrix_A:
+        print(i)
+
+    print() 
+    print('Matrix-B :')
+    for i in matrix_B:
+        print(i)
+
+        result = [[0 for j in range(matrix_B_cols)] for i in range(matrix_A_rows)]
+
+  
+    for i in range(len(matrix_A)):
+        for j in range(len(matrix_B[0])):
+            for k in range(len(matrix_B)):
+                result[i][j] += matrix_A[i][k] * matrix_B[k][j]
+        
+    print() 
+
+    print('Multiplication of Matrix-A and Matrix-B is :')
+    for i in result:  
+        print(i)
+        
+else:
+    print('Multiplication of matrices is not possible (columns of matrix-A = row of matrix-B)')
